4317346503188c71bc64613ff8975b7a8eb08cba
Kshitij <kshitijsaxena734@gmail.com>
2025-11-27 16:46:50 +0100

    Project structure is ready and virtual environment is set

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="This is end to end Mlops project",
+    author="Kshitij Saxena",
+    author_email="kshitijsaxena25@gmail.com",
+    packages=find_packages()
+    
+)
